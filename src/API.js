7f0dffaa09637d7273723e05cf88039d98dcb7cc
alinakyrianova <alina.kyrianova@gmail.com>
2023-08-30 11:44:40 +0200

    image.finder
    test

---
+++ src/API.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = '38291221-4c14b60a0e4e7e543a327e37e';
+axios.defaults.baseURL = `https://pixabay.com/api/`;
+
+export const fetchImages = async (query, page) => {
+  const separated = query.split('/');
+  const exstractedQuery = separated[1];
+  try {
+    const response = await axios.get('', {
+      params: {
+        key: API_KEY,
+        q: exstractedQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        page: page,
+        per_page: 12,
+      },
+    });
+    return response.data.hits;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
